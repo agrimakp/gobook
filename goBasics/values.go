70d6b9892825632c2d6bd24f4f24d961382e0976
Agrima KP <qvistn111@gmail.com>
2024-06-07 20:25:14 +1000

    values

---
+++ goBasics/values.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hello" + "Kitty")
+
+	fmt.Println("5+5 =", 5+5)
+	fmt.Println("9.0/2.0 =", 9.0/2.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+}
