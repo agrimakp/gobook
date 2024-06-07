6d88a8b7cd70c213505ce63c773e95979bb142b8
Agrima KP <qvistn111@gmail.com>
2024-06-07 21:19:15 +1000

    for without condition will loop repeatedly intil you break

--- goBasics/for.go
+++ goBasics/for.go
@@ -5,9 +5,25 @@ import "fmt"
 func main() {
 	i := 1
 
-	for i <= 3 {
+	count := 3
+
+	for i <= count {
 		fmt.Println(i)
 		i = i + 1
+	}
+
+	for j := 0; j < count; j++ {
+		fmt.Println(j)
+	}
+
+	for i := range 3 {
+		fmt.Println("range", i)
+	}
+
+	// for without a condition will loop repeatedly until you break out of the loop
+	for {
+		fmt.Println("loop repeatedly until you break")
+		break
 	}
 
 }
