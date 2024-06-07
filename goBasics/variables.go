3510b37a55ff2a740c74d4d9cfcd1108fc86d666
Agrima KP <qvistn111@gmail.com>
2024-06-07 20:27:27 +1000

    variables

---
+++ goBasics/variables.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "initial"
+	fmt.Println(a)
+	var g = 3
+	fmt.Println(g)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+	// 0
+	var h string
+	fmt.Println(h)
+	// empty
+	var i bool
+	fmt.Println(i)
+	// false
+
+	//  shorthand for declaring and initializing a variable
+	f := "hai"
+	fmt.Println(f)
+}
