32c5082cc877a5185a38144bf47b9a7598cb36fa
Agrima KP <qvistn111@gmail.com>
2024-06-08 22:01:43 +1000

    else if ladder

--- goBasics/ifElse.go
+++ goBasics/ifElse.go
@@ -20,4 +20,12 @@ func main() {
 		fmt.Println("odd")
 	}
 
+	if num := n; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
+
 }
