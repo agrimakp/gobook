9ce0d48f484c5a0fe519d9639295d933b68abfca
Agrima KP <qvistn111@gmail.com>
2024-06-09 07:15:18 +1000

    weekdat function in go

--- goBasics/switch.go
+++ goBasics/switch.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
@@ -16,4 +17,12 @@ func main() {
 	case 3:
 		fmt.Println("Three")
 	}
+
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's Working day")
+	}
+
 }
