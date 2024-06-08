28ce5e445703e37be796f1bfff0cad40d6e74950
Agrima KP <qvistn111@gmail.com>
2024-06-08 21:54:07 +1000

    if else

--- goBasics/ifElse.go
+++ goBasics/ifElse.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-
+	n := 7
 	// if without else
 	if 6%2 == 0 {
 		fmt.Println(" 6 is divisible by 2")
@@ -11,6 +11,13 @@ func main() {
 
 	if 3%2 == 0 || 8%2 == 0 {
 		fmt.Println("either 3 or 8 are even")
+	}
+
+	if n%2 == 0 {
+
+		fmt.Println("even")
+	} else {
+		fmt.Println("odd")
 	}
 
 }
